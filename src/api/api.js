69b5f1cc14ebc1125ecb4dce984c87e0b49e585d
R1Kka1 <2491911240@qq.com>
2026-08-13 08:38:52 +0800

    feat: 添加 Axios 统一错误处理

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,39 @@
 import axios from "axios"
 
+function handleError(error) {
+    if(error.response){
+        switch(error.response.status){
+            case 401 :
+                console.error("登录已过期");
+                localStorage.removeItem("user");
+                localStorage.removeItem("token");
+                break;
+
+            case 403 :
+                console.error("没有权限");
+                break;
+            case 404:
+                console.error("资源不存在");
+                break;
+
+            case 500:
+                console.error("服务器错误");
+                break;
+
+            default:
+                console.error("请求失败");
+        }
+    }else if (error.request){
+        console.error("网络连接失败");
+    }else{
+        console.error("请求发生错误");
+    }
+    
+}
+
 const request = axios.create({ baseURL:"http://localhost:3001"})
 
+//请求拦截器
 request.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem("token");
@@ -12,6 +44,16 @@ request.interceptors.request.use(
         return config;
     },
     (error) => {
+        return Promise.reject(error);
+    }
+);
+//响应拦截器
+request.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        handleError(error);
         return Promise.reject(error);
     }
 );
